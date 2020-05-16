cb1cd7d94dabb1eb1ad6830b947aaff09fc33c23
Holly Prothe <hollyprothe1@gmail.com>
2020-05-16 10:45:18 -0500

    start server js

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var reservations = [
+    {
+        routename: "wesley",
+        name: "Wesley",
+        phoneNumber: 4518526413,
+        email: 
+    }
+]
